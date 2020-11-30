0ba126277281a2e177f48cdfb9dc4fb61aa379d8
shwetank429 <68150586+shwetank429@users.noreply.github.com>
2020-11-30 18:42:20 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,22 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smoke = loadImage("sprites/smoke.png");
+    this.trail = [];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+    if(this.body.velocity.x>10 && this.body.position.x>200){
+    var pose = [this.body.position.x,this.body.position.y];
+    this.trail.push(pose);
+  }
+   for(var i=0;i<this.trail.length;i++){
+   image(this.smoke,this.trail[i][0],this.trail[i][1])
+   }
+  }
+}
